d86ef410642a9c883ece4baf278f4d6aa545bedf
Hao Liu <leomayleomay@gmail.com>
2013-05-14 12:49:58 +0800

    1, initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedLightExample::Application.config.secret_token = 'af6a816fc505a289613f972f91cabab51e5449939a5bbe697a117d4956d87bb73d850ffbdebbb55b4848523e1ce6fc12baddbb106e3197a4159d920cc164c5b0'
