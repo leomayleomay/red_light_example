3499297d7b2e27e1d1f811bdf4c40c1a1eb7b411
Hao Liu <leomayleomay@gmail.com>
2013-05-16 22:14:48 +0800

    1, revise the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,16 @@ class PostsController < ApplicationController
   before_filter :authenticate_user!
 
   def new
+    @post = current_user.posts.new
   end
 
   def create
+    @post = current_user.posts.new(params[:post])
+
+    if @post.save
+      redirect_to root_path, :notice => "Done"
+    else
+      render :new
+    end
   end
 end
