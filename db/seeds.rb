18d0f3447a8e046d485a969094624f83a78edb48
Hao Liu <leomayleomay@gmail.com>
2013-05-20 00:09:21 +0800

    1, fix the db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(:username => "johndoe", :password => "foobarbaz", :password_confirmation => "foobarbaz")
+User.create(:email => "johndoe@nowhere.com", :password => "foobarbaz", :password_confirmation => "foobarbaz")
